b1a06fbf459a438e6a21f0f3c18e118c177ae0f5
Dwi Nepatyaz <38588473+nepatyaz@users.noreply.github.com>
2020-05-11 16:21:02 +0700

    update

--- src/app/services/configuration.service.ts
+++ src/app/services/configuration.service.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@angular/core';
 import { configuration } from '../config/configuration';
 
 // created by Dwi S & Herza
-
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ConfigurationService {
-    private config: any = configuration;
-    getConfig() {
-        return this.config;
-    }
+  private config: any = configuration;
+  getConfig() {
+    return this.config;
+  }
 }
