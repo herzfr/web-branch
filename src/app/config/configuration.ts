ca2a9d7877c6267e1c611e503e39a4705b6c9383
Dwi Nepatyaz <38588473+nepatyaz@users.noreply.github.com>
2020-06-03 17:29:59 +0700

    update

--- src/app/config/configuration.ts
+++ src/app/config/configuration.ts
@@ -4,7 +4,6 @@ export const configuration = {
     ipServer: 'https://10.62.10.28:8443/',
     ipSocket: 'https://10.62.10.28:8444/',
 
-
     typeCheckSaldoGiro: "0001001",
     typeCheckSaldoTabungan: "0002001",
     typeSetorTunai: "0000002",
@@ -12,7 +11,5 @@ export const configuration = {
     typeTransferAntarRek: "0000004",
     typeTransferAntarBank: "0000005",
     typeNewAccount: "0000006",
-
-
 
 } 
