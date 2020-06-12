416195b7412b67c2613036c2955a48ee4a5a0791
Dwi Nepatyaz <38588473+nepatyaz@users.noreply.github.com>
2020-06-12 14:12:09 +0700

    update

--- src/app/models/UserData.ts
+++ src/app/models/UserData.ts
@@ -1,18 +1,18 @@
 
 export class UserData {
     userid: string;
-    username : string;
-    branchcode : string ;
-    firstname : string ;
-    lastname : string ;
+    username: string;
+    branchcode: string;
+    firstname: string;
+    lastname: string;
     email: string;
-    birthday : string ;
-    creationdate : string;
-    creationby : string ;
-    lastlogindate: string ;
-    enabled : number;
+    birthday: string;
+    creationdate: string;
+    creationby: string;
+    lastlogindate: string;
+    enabled: number;
     islogin: number;
-    imageid : string;
-    userterminal: number;
-    roles:any[];
+    imageid: string;
+    userterminal: string;
+    roles: any[];
 }
